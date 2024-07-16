94ce32e76b9388899812ce5952772e37257b91c0
miku <miku71041@gmail.com>
2024-07-17 01:58:00 +0900

    publicに

--- Battle.java
+++ Battle.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
-class Battle {
-    // バトルのロジックを実装
+public class Battle {
     public void fight(Player player, int enemyStrength) {
         player.setHp(player.getHp() - enemyStrength);
         player.setExperience(player.getExperience() + 100);  // 仮の経験値
